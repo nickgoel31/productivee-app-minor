3cc63d87945fd868db11a33a712cae19efa604ec
Harsh Goel <harshgoel2004@gmail.com>
2024-02-20 10:30:26 +0530

    sec commit

--- .history/app/(main)/workspace/[workspaceId]/_components/whiteboard/WhiteBoardBtn_20240220102933.tsx
+++ .history/app/(main)/workspace/[workspaceId]/_components/whiteboard/WhiteBoardBtn_20240220102933.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const WhiteBoardBtn = ({workspaceId} : {workspaceId : string}) => {
   return (
-    <Link href={`/whiteboard/${workspaceId}`} className='p-4 h-40 lg:w-48 w-full flex flex-col gap-3 items-center justify-center rounded-lg border bg-neutral-900 hover:text-green-500 hover:border-green-500 transition'>
+    <Link href={`/whiteboard/${workspaceId}`} className='p-4 h-40 lg:w-48 w-full flex flex-col gap-3 items-center justify-center rounded-lg border bg-neutral-900 transition'>
       <Presentation size={29}/>
       <h2 className="font-semibold text-xl">Whiteboard</h2>
     </Link>
